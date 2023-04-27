5a5c33ef21bda50424b8469cbf614819180065fa
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-04-27 17:08:48 +0100

    commit this

---
+++ java.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(".menu-toggler").on("click", function () {
+    $(this).toggleClass("open");
+    $(".top-nav").toggleClass("open");
+  });
+  $(".top-nav .nav-link").on("click", function () {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
+
+  AOS.init({
+    easing: "ease",
+    duration: 1800,
+    once: true,
+  });
+});
+
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $("#back2Top").fadeIn();
+  } else {
+    $("#back2Top").fadeOut();
+  }
+});
+$(document).ready(function () {
+  $("#back2Top").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+});
